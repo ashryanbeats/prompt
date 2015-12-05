cf654912e24e7cb3da73f2d54e7d320d11af7b7f
Ash Ryan <ash@ashryanbeats.com>
2015-12-05 14:44:30 -0500

    Button added for getting random prompt.

--- client/pre-build/get-prompt/get-prompt.controller.js
+++ client/pre-build/get-prompt/get-prompt.controller.js
@@ -1,21 +1,9 @@
 app.controller('PromptController', function($scope, $http, PromptFactory) {
-  
-  $scope.$on('$stateChangeSuccess', function () {
-    const defaultAllMessage = 'If you don\'t see a list of prompts here, you need to seed your database!\nIn your terminal, go to this app\'s directory and run `gulp seedDB`.\nThen try this page again.';
-    const defaultRandomMessage = 'Seed your database to get a random prompt!';
 
-    PromptFactory.getAllPrompts()
-      .then(function(prompts) {
-        
-        if (!prompts.length) {
-          $scope.defaultAllMessage = defaultMessage;
-        }
-        else {
-          $scope.prompts = prompts;
-        }
-        
-      });
+  const defaultRandomMessage = 'Error: we didn\'t get a prompt back.';
 
+  $scope.getRandomPrompt = function() {
+    
     PromptFactory.getRandomPrompt()
       .then(function(prompt) {
 
@@ -27,5 +15,6 @@ app.controller('PromptController', function($scope, $http, PromptFactory) {
         }
 
       });
-  });
+  }
+
 });
